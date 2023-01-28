a5a219fb391e34fd9b776e1d909419c3861ea0e8
liamtran <quyettk@zen-s.com>
2023-01-28 20:17:52 +0700

    remove button test dark mode

--- src/app/(user)/page.jsx
+++ src/app/(user)/page.jsx
@@ -1,36 +1,30 @@
 'use client';
 
 import '../../styles/globals.css';
-// import { previewData } from 'next/headers';
-// import PreviewSuspense from '../../components/PreviewSuspense';
-// import groq from 'groq';
-import { useTheme } from 'next-themes';
+import { previewData } from 'next/headers';
+import PreviewSuspense from '../../components/PreviewSuspense';
+import groq from 'groq';
 
-// export const query = groq`
-// *[_type == "post"]{
-//   title,
-//   content,
-//   coverImage,
-//   date
-// } | order(date desc)
-// `;
+export const query = groq`
+*[_type == "post"]{
+  title,
+  content,
+  coverImage,
+  date
+} | order(date desc)
+`;
 
 export default function Home() {
-  const { theme, setTheme } = useTheme();
-
-  // if (previewData()) {
-  //   return (
-  //     <PreviewSuspense fallback="Loading...">
-  //       <div className="">this is preview mode</div>
-  //     </PreviewSuspense>
-  //   );
-  // }
+  if (previewData()) {
+    return (
+      <PreviewSuspense fallback="Loading...">
+        <div className="">this is preview mode</div>
+      </PreviewSuspense>
+    );
+  }
 
   return (
     <>
-      The current theme is: {theme}
-      <button onClick={() => setTheme('light')}>Light Mode</button>
-      <button onClick={() => setTheme('dark')}>Dark Mode</button>
       <div>this is not preview mode</div>
     </>
   );
